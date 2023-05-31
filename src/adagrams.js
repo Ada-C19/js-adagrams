60dfe22c78720fe197f3e95f648d784331690649
Laura <lauraemiliaperez@outlook.com>
2023-05-30 19:04:51 -0700

    wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,85 @@
+const letterHand = 10
+const letterPool = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+function generateRandomLetter() {
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+  return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+function countOccurrences(array, element) {
+  let count = 0
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === element) {
+      count++;
+    }
+  }
+  return count
+}
+
+function checkForValues(object, valueOccurernces, letter) {
+  if (object[letter] >= valueOccurernces) {
+    console.log(true)
+      return true
+  }
+  else {
+    return false
+  }
+}
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const handList = []
+  while (handList.length < letterHand) {
+      const randomLetter = generateRandomLetter();
+      handList.push(randomLetter);
+    const valueInArray = countOccurrences(handList, randomLetter)
+    console.log(valueInArray)
+    const valueEnoughTimes = checkForValues(letterPool, valueInArray, randomLetter)
+    
+      if (valueEnoughTimes === true) { continue; }
+      else {
+        handList.pop()
+      }
+  }
+  return handList
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+// };
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
+
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
+// };
