9b9fc6d3a5b09fca02045ffa89af1d5cf1135ea0
halimAremu <halimat3000@gmail.com>
2023-06-01 17:33:32 -0500

    started wave 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,30 +1,12 @@
 const LETTER_POOL = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1,
+  A: 9,B: 2,C: 2,D: 4,E: 12,F: 2,G: 3,H: 2,I: 9,J: 1,K: 1,L: 4,
+  M: 2,N: 6,O: 8,P: 2,Q: 1,R: 6,S: 4,T: 6,U: 4,V: 2,W: 2,X: 1,
+  Y: 2,Z: 1,
+};
+const LETTER_SCORE = {
+  A: 1,B: 3,C: 3,D: 2,E: 1,F: 4,G: 2,H: 4,I: 1,J: 8,K: 5,L: 1,
+  M: 3,N: 1,O: 1,P: 3,Q: 10,R: 1,S: 1,T: 1,U: 1,V: 4,W: 4,X: 8,
+  Y: 4,Z: 10,
 };
 
 
@@ -57,9 +39,41 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let sum = 0;
+  word = word.toUpperCase();
+  for(let char of word){
+    sum += LETTER_SCORE[char];
+
+  }
+  if(word.length >6){
+    sum += 8;
+  }
+  return sum;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let scoreList = []
+  for(let word of words){
+    let score = scoreWord(word);
+    scoreList.push(score);
+  }
+  const higestScore = Math.max(scoreList);
+  let highestWord;
+  for(let word of words){
+    if(scoreWord(word)=== higestScore && word.length === 10){
+        highestWord = word;
+    }
+    else{
+      let sortedWords = words.sort((x,y) => x.length-y.length)
+      for (let word of sortedWords){
+        if(scoreWord(word)=== higestScore){
+          highestWord = word;
+          break;
+        }
+      }
+    }
+  }
+  let maxWordObj = {}
+  maxWordObj[highestWord] = higestScore;
+  return maxWordObj;
 };
