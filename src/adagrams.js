13f77e0d6eba2c6f6a4ddd09b22f173242924664
dreinicke27 <danicareinicke@gmail.com>
2023-06-01 13:25:30 -0700

    Added pseudocode and real code for highestScoreFrom function to pass tests. Added an expect() to the test that was missing it

--- src/adagrams.js
+++ src/adagrams.js
@@ -94,5 +94,48 @@ export const scoreWord = (word) => {
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  // Pseudocode: (instead - make a list of objects with word and score and length. then filter and map?)
+  // 1. use a loop to score each of the words in the input array, save the output in an 
+  // array of objects wwhere each object has keys word, score, and length with corresponding values
+  // 2. use the .max and .map function to get the max score from the array
+  // 3. use .filter to move all objects whose score is the high score to a new array 
+  // 4. if there's more than one element in that array, apply tie breaking logic. if not,
+  // return the single element in that array 
+  // 5. use .max and .apply functions to get the max word length from the array 
+  // 6. if the max is less than 10, use .min and .apply functions to get the min word 
+  // length from the array 
+  // 7. use .filter and .map to add all objects with the min length to a new array, 
+  // and return the first element of that list 
+  // 8. if the max is greater than or equal to 10, use .filter and .map to add all 
+  // objects with the max length to a new array, and return the first element of that list
+ 
+  const scoredWords = [];
+
+  for (const word of words) {
+    scoredWords.push({
+      "word": word,
+      "score": scoreWord(word),
+      "length": word.length
+    });
+  }
+
+  const highScore = Math.max(...scoredWords.map(w => w.score));
+  const highScoreWords = scoredWords.filter(s => s.score === highScore);
+
+  if (highScoreWords.length === 1) {
+    return {"word": highScoreWords[0].word, "score": highScoreWords[0].score};
+  }
+
+  const longestWordLength =  Math.max(...highScoreWords.map(w => w.length));
+  let possibleWinners = [];
+  
+  if (longestWordLength < 10) {
+    const shortestWordLength = Math.min(...highScoreWords.map(w => w.length));
+    possibleWinners = highScoreWords.filter(l => l.length === shortestWordLength);
+    return {"word": possibleWinners[0].word, "score": possibleWinners[0].score};
+  } 
+
+  possibleWinners = highScoreWords.filter(l => l.length === longestWordLength);
+  return {"word": possibleWinners[0].word, "score": possibleWinners[0].score};
+
 };
