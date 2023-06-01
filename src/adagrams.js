58c378c7f9d9f0cf1ed76d1d5c62b77b39a57138
Lyuda Kim <lyudarkim@gmail.com>
2023-05-31 19:37:28 -0700

    Implement helper function generateLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,73 @@
-export const drawLetters = () => {
-  // Implement this method for wave 1
+const drawLetters = () => {
+  
+  const letters = generateLetters();
+  
+  // Shuffle the letters
+  
+  // Draw from the top (get the last item in the array)
+  
+  
+  
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+// Helper functions:
+const generateLetters = () => {
+  // Add all letters however many times they should appear to a new array
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+  const LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+    }
+  const letters = [];
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
+  for (const letter in LETTER_POOL) {
+    const frequency = LETTER_POOL[letter]
+    for (let i = 0; i < frequency; i++) {
+      letters.push(letter);
+    }
+  }
+
+  return letters;
+}
+
+const shuffleLetters = (letters) => {
+  return letters.sort( () => Math.random()-0.5);
+}
+
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+// };
+
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
+
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
+// };
+
+console.log(drawLetters());
