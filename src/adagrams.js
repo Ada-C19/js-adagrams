78c4fd78d8af0fc5913930b1808070dddc2cbd70
Gabby <gabrielleyoung10@gmail.com>
2023-06-01 21:29:16 -0500

    Complete Wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,23 @@
+import { LETTER_POOL } from './constants';
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let lettersArr = [];
+  for (const [letter, count] of Object.entries(LETTER_POOL)) {
+    const newLetters = lettersArr.concat(Array(count).fill(letter));
+    lettersArr = newLetters;
+  }
+
+  const hand = [];
+  const usedIndices = [];
+  for (let i = 0; i < 10; i++) {
+    const randIndex = Math.floor(Math.random() * lettersArr.length);
+    if (!usedIndices.includes(randIndex)) {
+      hand.push(lettersArr[randIndex]);
+      usedIndices.push(randIndex);
+    }
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
