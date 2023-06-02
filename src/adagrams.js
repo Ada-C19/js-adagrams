6ce44fa8d4324339d9188b6b96c98c23cb62ac9e
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-06-02 06:11:20 -0400

    Complete Optional Wave 5 encapsulating the functions in a class

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,146 +27,293 @@ const letterPool = {
   Z: 1,
 };
 
-//helper function
-const randomSelectedLetter = () =>{
-    const lettersAlphabet = Object.keys(letterPool);
-    const randomIndex = Math.floor(Math.random()*lettersAlphabet.length-1);
-    const randomLetter = lettersAlphabet[randomIndex];
-    return randomLetter;
+class Adagrams {
+    //helper function
+    static randomSelectedLetter (){
+        const lettersAlphabet = Object.keys(letterPool);
+        const randomIndex = Math.floor(Math.random()*lettersAlphabet.length-1);
+        const randomLetter = lettersAlphabet[randomIndex];
+        return randomLetter;
+    }
+    static drawLetters() {
+        // Implement this method for wave 1
+        let hand= [];
+        let randomLetter = '';
+
+        while (hand.length < 10){
+            randomLetter = Adagrams.randomSelectedLetter();
+            const countLetter= hand.filter(i => i === randomLetter).length;
+            if(countLetter < letterPool[randomLetter]){
+                hand.push(randomLetter);
+                }
+            }
+        return hand;
+    }
+
+    //helper function
+    static wordLetterDictObject(dataToConvert){
+        const dictObjectConvert = {}
+
+        for(let elem of dataToConvert){
+            if(elem in dictObjectConvert){
+                dictObjectConvert[elem] += 1;
+                }else{
+                    dictObjectConvert[elem] = 1;
+                }
+            }
+        return dictObjectConvert;
+    }
+
+    static usesAvailableLetters(input, lettersInHand) {
+        // Implement this method for wave 2
+        let valid = true;
+        const wordUpper = input.toUpperCase();
+        const dictWord = Adagrams.wordLetterDictObject(wordUpper);
+        const dictLettersInHand = Adagrams.wordLetterDictObject(lettersInHand);
+
+        for(let elem in dictWord){
+            if(!(elem in dictLettersInHand) || dictWord[elem] > dictLettersInHand[elem]){
+                valid = false;
+                break;
+            }
+        }
+        return valid;
+    }
+
+    static scoreWord (word){
+        // Implement this method for wave 3
+        const dictScore = {
+                  A:1,
+                  B:3,
+                  C:3,
+                  D:2,
+                  E:1,
+                  F:4,
+                  G:2,
+                  H:4,
+                  I:1,
+                  J:8,
+                  K:5,
+                  L:1,
+                  M:3,
+                  N:1,
+                  O:1,
+                  P:3,
+                  Q:10,
+                  R:1,
+                  S:1,
+                  T:1,
+                  U:1,
+                  V:4,
+                  W:4,
+                  X:8,
+                  Y:4,
+                  Z:10
+                }
+
+        //score variable to store the points earned
+        let result = 0;
+
+        //wordUpper variable converts word variable into uppercase letters
+        const wordUpper = word.toUpperCase();
+
+        for(let letter of wordUpper){
+            for(let[letters, score] of Object.entries(dictScore)){
+                if(letters.includes(letter)){
+                    result += score;
+                }
+            }
+        }
+
+        if(word.length >= 7 && word.length <=10){
+            result += 8;
+        }
+
+        return result;
+    }
+
+    static highestScoreFrom(words){
+        // Implement this method for wave 4
+        let highestScoreResult= {};
+        const scoreWordsDict = {};
+        let score = 0;
+
+        for(let word of words){
+            score = Adagrams.scoreWord(word);
+            scoreWordsDict[word]=score;
+        }
+
+        //get the highest value
+        let highestValue = Object.values(scoreWordsDict)[0];
+        for(const score of Object.values(scoreWordsDict)){
+            if(score > highestValue){
+                highestValue = score;
+            }
+        }
+        //get an array of highest words
+        let listHighestWords = []
+        for(const[word, score] of Object.entries(scoreWordsDict)){
+            if(score === highestValue){
+                listHighestWords.push(word);
+            }
+        }
+
+        //get the best word from the list of highest words
+        let bestWord = listHighestWords[0];
+        for(let word of listHighestWords){
+            if(word.length === 10){
+                bestWord = word;
+                break;
+            }else if(word.length < bestWord.length){
+                bestWord = word;
+            }
+        }
+        highestScoreResult = {word:bestWord, score: highestValue};
+        return highestScoreResult;
+    }
 }
-export const drawLetters = () => {
-  // Implement this method for wave 1
-    let hand= [];
-    let randomLetter = '';
 
-    while (hand.length < 10){
-        randomLetter = randomSelectedLetter();
-        const countLetter= hand.filter(i => i === randomLetter).length;
-        if(countLetter < letterPool[randomLetter]){
-            hand.push(randomLetter);
-        }
-    }
-    return hand;
-};
+export default Adagrams;
+// //helper function
+// const randomSelectedLetter = () =>{
+//     const lettersAlphabet = Object.keys(letterPool);
+//     const randomIndex = Math.floor(Math.random()*lettersAlphabet.length-1);
+//     const randomLetter = lettersAlphabet[randomIndex];
+//     return randomLetter;
+// }
+// export const drawLetters = () => {
+//   // Implement this method for wave 1
+//     let hand= [];
+//     let randomLetter = '';
 
-//helper function
-const wordLetterDictObject=(dataToConvert)=>{
-  const dictObjectConvert = {}
+//     while (hand.length < 10){
+//         randomLetter = randomSelectedLetter();
+//         const countLetter= hand.filter(i => i === randomLetter).length;
+//         if(countLetter < letterPool[randomLetter]){
+//             hand.push(randomLetter);
+//         }
+//     }
+//     return hand;
+// };
 
-  for(let elem of dataToConvert){
-      if(elem in dictObjectConvert){
-          dictObjectConvert[elem] += 1;
-      }else{
-          dictObjectConvert[elem] = 1;
-      }
-  }
-  return dictObjectConvert;
-};
+// //helper function
+// const wordLetterDictObject=(dataToConvert)=>{
+//   const dictObjectConvert = {}
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-    let valid = true;
-    const wordUpper = input.toUpperCase();
-    const dictWord = wordLetterDictObject(wordUpper);
-    const dictLettersInHand = wordLetterDictObject(lettersInHand);
+//   for(let elem of dataToConvert){
+//       if(elem in dictObjectConvert){
+//           dictObjectConvert[elem] += 1;
+//       }else{
+//           dictObjectConvert[elem] = 1;
+//       }
+//   }
+//   return dictObjectConvert;
+// };
 
-    for(let elem in dictWord){
-        if(!(elem in dictLettersInHand) || dictWord[elem] > dictLettersInHand[elem]){
-            valid = false;
-            break;
-        }
-    }
-    return valid;
-};
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+//     let valid = true;
+//     const wordUpper = input.toUpperCase();
+//     const dictWord = wordLetterDictObject(wordUpper);
+//     const dictLettersInHand = wordLetterDictObject(lettersInHand);
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-  const dictScore = {
-    A:1,
-    B:3,
-    C:3,
-    D:2,
-    E:1,
-    F:4,
-    G:2,
-    H:4,
-    I:1,
-    J:8,
-    K:5,
-    L:1,
-    M:3,
-    N:1,
-    O:1,
-    P:3,
-    Q:10,
-    R:1,
-    S:1,
-    T:1,
-    U:1,
-    V:4,
-    W:4,
-    X:8,
-    Y:4,
-    Z:10
-  }
+//     for(let elem in dictWord){
+//         if(!(elem in dictLettersInHand) || dictWord[elem] > dictLettersInHand[elem]){
+//             valid = false;
+//             break;
+//         }
+//     }
+//     return valid;
+// };
 
-  //score variable to store the points earned
-  let result = 0;
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+//   const dictScore = {
+//     A:1,
+//     B:3,
+//     C:3,
+//     D:2,
+//     E:1,
+//     F:4,
+//     G:2,
+//     H:4,
+//     I:1,
+//     J:8,
+//     K:5,
+//     L:1,
+//     M:3,
+//     N:1,
+//     O:1,
+//     P:3,
+//     Q:10,
+//     R:1,
+//     S:1,
+//     T:1,
+//     U:1,
+//     V:4,
+//     W:4,
+//     X:8,
+//     Y:4,
+//     Z:10
+//   }
 
-  //wordUpper variable converts word variable into uppercase letters
-  const wordUpper = word.toUpperCase();
+//   //score variable to store the points earned
+//   let result = 0;
 
-  for(let letter of wordUpper){
-      for(let[letters, score] of Object.entries(dictScore)){
-          if(letters.includes(letter)){
-              result += score;
-          }
-      }
-  }
+//   //wordUpper variable converts word variable into uppercase letters
+//   const wordUpper = word.toUpperCase();
 
-  if(word.length >= 7 && word.length <=10){
-      result += 8;
-  }
+//   for(let letter of wordUpper){
+//       for(let[letters, score] of Object.entries(dictScore)){
+//           if(letters.includes(letter)){
+//               result += score;
+//           }
+//       }
+//   }
 
-  return result;
-};
+//   if(word.length >= 7 && word.length <=10){
+//       result += 8;
+//   }
 
-export const highestScoreFrom = (words) => {
-// Implement this method for wave 4
-  let highestScoreResult= {};
-  const scoreWordsDict = {};
-  let score = 0;
+//   return result;
+// };
 
-  for(let word of words){
-      score = scoreWord(word);
-      scoreWordsDict[word]=score;
-  }
+// export const highestScoreFrom = (words) => {
+// // Implement this method for wave 4
+//   let highestScoreResult= {};
+//   const scoreWordsDict = {};
+//   let score = 0;
 
-  //get the highest value
-  let highestValue = Object.values(scoreWordsDict)[0];
-  for(const score of Object.values(scoreWordsDict)){
-      if(score > highestValue){
-          highestValue = score;
-      }
-  }
-  //get an array of highest words
-  let listHighestWords = []
-  for(const[word, score] of Object.entries(scoreWordsDict)){
-      if(score === highestValue){
-          listHighestWords.push(word);
-      }
-  }
+//   for(let word of words){
+//       score = scoreWord(word);
+//       scoreWordsDict[word]=score;
+//   }
 
-  //get the best word from the list of highest words
-  let bestWord = listHighestWords[0];
-  for(let word of listHighestWords){
-      if(word.length === 10){
-          bestWord = word;
-          break;
-      }else if(word.length < bestWord.length){
-          bestWord = word;
-      }
-  }
-  highestScoreResult = {word:bestWord, score: highestValue};
-  return highestScoreResult;
-};
+//   //get the highest value
+//   let highestValue = Object.values(scoreWordsDict)[0];
+//   for(const score of Object.values(scoreWordsDict)){
+//       if(score > highestValue){
+//           highestValue = score;
+//       }
+//   }
+//   //get an array of highest words
+//   let listHighestWords = []
+//   for(const[word, score] of Object.entries(scoreWordsDict)){
+//       if(score === highestValue){
+//           listHighestWords.push(word);
+//       }
+//   }
+
+//   //get the best word from the list of highest words
+//   let bestWord = listHighestWords[0];
+//   for(let word of listHighestWords){
+//       if(word.length === 10){
+//           bestWord = word;
+//           break;
+//       }else if(word.length < bestWord.length){
+//           bestWord = word;
+//       }
+//   }
+//   highestScoreResult = {word:bestWord, score: highestValue};
+//   return highestScoreResult;
+// };
