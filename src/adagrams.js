5a242e333650cdd33f1b04e0f731b6ffea77cfc9
Carline <carlinesenat@gmail.com>
2023-06-01 22:17:15 -0700

    Wave 1 complete

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,54 @@
+const letterPool = {
+  'A': 9, 
+  'B': 2, 
+  'C': 2, 
+  'D': 4, 
+  'E': 12, 
+  'F': 2, 
+  'G': 3, 
+  'H': 2, 
+  'I': 9, 
+  'J': 1, 
+  'K': 1, 
+  'L': 4, 
+  'M': 2, 
+  'N': 6, 
+  'O': 8, 
+  'P': 2, 
+  'Q': 1, 
+  'R': 6, 
+  'S': 4, 
+  'T': 6, 
+  'U': 4, 
+  'V': 2, 
+  'W': 2, 
+  'X': 1, 
+  'Y': 2, 
+  'Z': 1
+}
+//fn to create a letter_pile:
 export const drawLetters = () => {
   // Implement this method for wave 1
+  // const letterPile = Object.keys(letterPool)
+  // const hand = [];
+  
+  let pool = {...letterPool};
+  let hand = [];
+
+// for (let i = 0; i < 10; i++) {
+  while (hand.length < 10) {
+  const letters = Object.keys(letterPool);
+  const letter = letters[Math.floor(Math.random() * letters.length)];
+
+  if (pool[letter] > 0) {
+    hand.push(letter);
+    pool[letter] -= 1;
+  // } else {
+  //   delete pool[letter];
+}
+
+}
+return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
