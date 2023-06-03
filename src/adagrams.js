3704e94b891166b11ffa22fcd7b67ee119741f2a
Jasmin Worthy <jasminworthy@gmail.com>
2023-06-02 19:20:48 -0500

    create scoreword to pass wave 3 test

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,7 +26,7 @@ export const drawLetters = () => {
     'X': 1, 
     'Y': 2, 
     'Z': 1
-}
+    }
   let hand = [];
   let randomLetter = "";
   while (hand.length < 10) {
@@ -63,9 +63,49 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
+export const scoreWord = (word) => {
+    const chart = {
+      'A': 1,
+      'E': 1,
+      'I': 1,
+      'O': 1,
+      'U': 1,
+      'L': 1,
+      'N': 1,
+      'R': 1,
+      'S': 1,
+      'T': 1,
+      'D': 2,
+      'G': 2,
+      'B': 3,
+      'C': 3,
+      'M': 3,
+      'P': 3,
+      'F': 4,
+      'H': 4,
+      'V': 4,
+      'W': 4,
+      'Y': 4,
+      'K': 5,
+      'J': 8,
+      'X': 8,
+      'Q': 10,
+      'Z': 10
+    };
+  
+    let score = 0;
+    word = word.toUpperCase();
+  
+    for (let letter of word) {
+      score += chart[letter.toUpperCase()];
+    }
+  
+    if (word.length >= 7 && word.length <= 10) {
+      score += 8;
+    }
+  
+    return score;
+};
 
 // export const highestScoreFrom = (words) => {
 //   // Implement this method for wave 4
