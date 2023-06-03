68493f1cdde95320a79961f3f626af4c53dc0d80
Ana Villalobos <ana.villalobos309@gmail.com>
2023-06-02 23:54:53 -0700

    Created scoreWord function and passed all associated tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,34 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const score_pool = {
+  'A': 1, 
+  'B': 3, 
+  'C': 3, 
+  'D': 2, 
+  'E': 1, 
+  'F': 4, 
+  'G': 2, 
+  'H': 4, 
+  'I': 1, 
+  'J': 8, 
+  'K': 5, 
+  'L': 1, 
+  'M': 3, 
+  'N': 1, 
+  'O': 1, 
+  'P': 3, 
+  'Q': 10, 
+  'R': 1, 
+  'S': 1, 
+  'T': 1, 
+  'U': 1, 
+  'V': 4, 
+  'W': 4, 
+  'X': 8, 
+  'Y': 4, 
+  'Z': 10
+};
 
 export const drawLetters = () => {
   // Implement this method for wave 1
@@ -59,8 +87,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+  let score = 0;
+
+  for (let char of word.toUpperCase()) {
+    score += score_pool[char];
+  }
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8;
+  } return score;
+  };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
