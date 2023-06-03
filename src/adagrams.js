c42a80ddf08ffc6789a307734ab9140e7dbf9d9c
Carline <carlinesenat@gmail.com>
2023-06-02 23:50:27 -0700

    Completed Wave 4 + Test!

--- src/adagrams.js
+++ src/adagrams.js
@@ -118,4 +118,30 @@ export const scoreWord = (word) => {
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+//   - Has one parameter: `words`, which is an array of strings
+// - Returns a single object that represents the data of a winning word and its score. The object should have the following keys:
+//   - `word`, whose value is a string of a word
+//   - `score`, whose value is the score of that word
+// - In the case of tie in scores, use these tie-breaking rules:
+//   - prefer the word with the fewest letters...
+//   - ...unless one word has 10 letters. If the top score is tied between multiple words and one is 10 letters long, choose the one with 10 letters over the one with fewer tiles
+//   - If the there are multiple words that are the same score and the same length, pick the first one in the supplied list
+  let highScore = 0;
+  let highWord = '';
+  for (let word of words) {
+    let score = scoreWord(word);
+    if (score > highScore) {
+      highScore = score;
+      highWord = word
+    } else if (score == highScore) {
+        if (highWord.length === 10) {
+          continue
+
+        }else if (word.length === 10 || word.length < highWord.length) {
+          highScore = score;
+          highWord = word;
+        } 
+      }
+    }
+    return {word: highWord, score: highScore};
 };
