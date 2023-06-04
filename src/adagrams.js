a370de0e7c14616fa63c75008b2edfdca84dbf04
Nadia Mirzai <nadia.m.mirzai@gmail.com>
2023-06-03 20:41:43 -0400

    Added scoreWord function, added two unit tests, wave 3 passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,5 @@
+// Wave 1:
 export const drawLetters = () => {
-  // Implement this method for wave 1
   let weightedPool = [];
   let numberOfLetters = 0;
   let handOfLetters = [];
@@ -25,7 +25,7 @@ export const drawLetters = () => {
     weightedPool.splice(weightedPool.indexOf(letter), 1)
   }
   return handOfLetters
-}
+};
 
 // Wave 2:
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -40,66 +40,20 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
-// def uses_available_letters(word, letter_bank):
-// word_upper = word.upper()
-
-// for letter in list(word_upper):
-//   if word_upper.count(letter) == letter_bank.count(letter):
-//     result = True
-//   else:
-//   result = False
-// return result
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
-
-// export const highestScoreFrom = (words) => {
-//   // Implement this method for wave 4
-// };
-
-/*
-
-
-
-
-
-def score_word(word):
-  total_score = 0
-  score_chart = {
-      'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1,
-      'N': 1, 'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2,
-      'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4,
-      'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8,
-      'Q': 10, 'Z': 10
-
+// Wave 3:
+export const scoreWord = (word) => {
+  const scoreChart = {
+    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1,
+    N: 1, R: 1, S: 1, T: 1, D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3, F: 4, H: 4,
+    V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8,
+    Q: 10, Z: 10
   }
-  for str in word:
-      str = str.upper()
-      total_score += score_chart[str]
-  if len(word) >= 7:
-      total_score += 8
-  return total_score
 
+  let totalScore = 0;
+  word = word.toUpperCase();
+  for (let str of word) totalScore += scoreChart[str];
+  if (word.length >= 7) totalScore += 8;
+  return totalScore
+};
 
-def get_highest_word_score(word_list):
-  highest_word_score = 0
-  counter = 0
-  best_word = word_list[counter]
-
-  for word in word_list:
-      score = score_word(word_list[counter])
-
-      if score > highest_word_score:
-          highest_word_score = score
-          best_word = word
-
-      elif score == highest_word_score:
-          if len(word) == 10 and len(best_word) != 10:
-              best_word = word
-          elif len(word) < len(best_word) and len(best_word) != 10:
-              best_word = word
-
-      counter += 1
-
-  return (best_word, highest_word_score)
-  */
