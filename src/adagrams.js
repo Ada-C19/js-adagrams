95d71c694404a22cd46d00ef3fe9fd5fb12d5207
Julie Loh <jlohusky@gmail.com>
2023-06-04 22:49:57 +0800

    wave 3 implementation

--- src/adagrams.js
+++ src/adagrams.js
@@ -64,12 +64,10 @@ export const usesAvailableLetters = (input, lettersInHand) => {
       letterPool[letter] = 1;
     }
   })
-  console.log(letterPool)
 
   inputArray.forEach((letter) => {
     if (letter in letterPool && letterPool[letter] > 0) {
       letterPool[letter] -= 1;
-      console.log(letterPool)
     } else {
       result = false;
     }
@@ -78,9 +76,49 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return result;
 };
 
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
+export const scoreWord = (word) => {
+  // Implement this method for wave 3
+  const scoreChart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+  const wordLen = [7, 8, 9, 10]
+  const wordArray = word.toUpperCase().split('');
+  let score = 0;
+
+  wordArray.forEach((letter) => {
+    score += scoreChart[letter];
+  });
+
+  if (wordLen.includes(wordArray.length)) {
+    score += 8;
+  }
+  return score;
+};
 
 // export const highestScoreFrom = (words) => {
 //   // Implement this method for wave 4
