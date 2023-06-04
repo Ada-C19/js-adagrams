4b179558bf5e5840478394b8b5e096f822a0c206
Yulia S <startyupp@gmail.com>
2023-06-03 23:01:48 -0700

    Created a helper function that randomly returns an index andand add letter by that index to the letterBank. invoke helper funtion in main and return the result.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,10 +1,28 @@
 import { LETTER_POOL, LETTER_COST } from './letter-bank-cost';
+/*const m = require('./letter-bank-cost');
+const LETTER_COST = m.LETTER_COST;
+const LETTER_POOL = m.LETTER_POOL;
+*/
+
+const randomLetters = letters => {
+  const letterBank = [];
+  let indexSet = new Set();
+  while (indexSet.size < 10) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    if (!indexSet.has(randomIndex)) {
+      indexSet.add(randomIndex);
+      letterBank.push(letters[randomIndex]);
+    }
+  }
+  return letterBank;
+};
 
 export const drawLetters = () => {
-  let letters = [];
+  let letters = '';
   for (let [letter, count] of Object.entries(LETTER_POOL)) {
-    letters.push(letter.repeat(count));
+    letters += letter.repeat(count);
   }
+  return randomLetters(letters);
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
