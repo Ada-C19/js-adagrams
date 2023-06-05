0e2c432e9e785b17242cb6eb54a5a5a1bf0de826
nadia <reach.nadia.saria@gmail.com>
2023-06-05 15:59:51 -0700

    finish wave 01

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,10 +1,32 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
-};
+  const LETTER_POOL = {
+    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 
+    'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 
+    'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 
+    'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 
+    'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 
+    'Z': 1
+  };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+  const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+  
+  let letterBag = [];
+  for (const letter in LETTER_POOL) {
+    for (let i = 0; i < LETTER_POOL[letter]; i++) {
+      letterBag.push(letter);
+    }
+  }
+  shuffleArray(letterBag);
+  const playerHand = letterBag.slice(0, 10);
+  return playerHand;
+}
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
