f52cabff57f3e2563a02f6924d4bda3dfaeeb500
Yulia S <startyupp@gmail.com>
2023-06-04 18:36:28 -0700

    Completed empty input test case for wave 3, all tests for wave 3 passed

--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -119,9 +119,8 @@ describe('Adagrams', () => {
       });
     });
 
-    it('returns a score of 0 if given an empty input', () => {
-      throw 'Complete test';
-    });
+    it('returns a score of 0 if given an empty input', () =>
+      expectScores({ '': 0 }));
 
     it('adds an extra 8 points if word is 7 or more characters long', () => {
       expectScores({
