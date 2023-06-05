f6c9f965b8302ec80bc9e7eac3be0e2be1d0f70a
Winslow <owedwards@gmail.com>
2023-06-05 18:29:54 -0400

    drawLetters written, not passing tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,59 @@
-export const drawLetters = () => {
-  // Implement this method for wave 1
+// export 
+const drawLetters = () => {
+  const letterPool = {
+    A : 9,
+    B : 2,
+    C : 2,
+    D : 4,
+    E : 12,
+    F : 2,
+    G : 3,
+    H : 2,
+    I : 9,
+    J : 1,
+    K : 1,
+    L : 4,
+    M : 2,
+    N : 6,
+    O : 8,
+    P : 2,
+    Q : 1,
+    R : 6,
+    S : 4,
+    T : 6,
+    U : 4,
+    V : 2,
+    W : 2,
+    X : 1,
+    Y : 2,
+    Z : 1
+  }
+  
+  const letterPoolArray = []
+  const lettersInHand = []
+
+  for (let letter in letterPool) {
+    for (let i = 0; i < letterPool[letter]; i++) {
+      letterPoolArray.push(letter);
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    let randomLetter = letterPoolArray[Math.floor(Math.random() * letterPoolArray.length)]
+    lettersInHand.push(randomLetter);
+  }
+
+  return lettersInHand
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+// };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
+// };
