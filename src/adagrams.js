daa5a7c8ae6cd4bfc9f35828bfe52c0c1dafc903
Kat <kmunson13@gmail.com>
2023-06-05 19:02:57 -0400

    Finish wave 1, add 10 letter hand generator functions

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,59 @@
+const letterPool = {
+  "A": 9,
+  "B": 2,
+  "C": 2,
+  "D": 2,
+  "E": 12,
+  "F": 2,
+  "G": 3,
+  "H": 2,
+  "I": 9,
+  "J": 1,
+  "K": 1,
+  "L": 4,
+  "M": 2,
+  "N": 6,
+  "O": 8,
+  "P": 2,
+  "Q": 1,
+  "R": 6,
+  "S": 4,
+  "T": 6,
+  "U": 4,
+  "V": 2,
+  "W": 2,
+  "X": 1,
+  "Y": 2,
+  "Z": 1
+}
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const generatedPool = generatePool();
+  const hand = []
+
+  function generatePool() {
+    const newArr = [];
+    Object.entries(letterPool).forEach(([key, value]) => {
+      newArr.push(repeatElement(key, value));
+    });
+    return newArr.flat();
+  }
+
+  function repeatElement(element, count) {
+    return Array(count).fill(element).flat();
+  }
+
+  function drawLetter() {
+    let randomNumber = Math.floor(Math.random() * generatedPool.length)
+    hand.push(generatedPool[randomNumber])
+    generatedPool.splice(randomNumber, 1)
+  }
+
+  while (hand.length < 10) {
+    drawLetter()
+  }
+  
+  return hand
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
