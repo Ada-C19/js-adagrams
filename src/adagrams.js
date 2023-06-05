64e24d81aebc7771a0b2e89fd0a1ad90c0222565
Jennifer Tam <jenniferwtam@gmail.com>
2023-06-04 21:00:28 -0400

    completed test for wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -75,6 +75,9 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   };
 
 export const scoreWord = (word) => {
+  if (word === "") {
+    return 0;
+    }
   // Define the points for each letter
   const letterPoints = {
     A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5,
