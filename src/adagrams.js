e3be2ab88faf82124095bb99e3d60e720506cb60
Valerie Valentin <valerie.valentine93@gmail.com>
2023-06-06 09:14:01 -0700

    Finished wave 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,13 +1,15 @@
+const LETTER_POOL = {A:9, B:2, C:2, D:4, E:12, F:2, G:3, H:2, I:9, J:1, K:1, L:4,  
+  M:2, N:6, O:8, P:2, Q:1, R:6, S:4, T:6, U:4, V:2, W:2, X:1, Y:2, Z:1};
+
 export const drawLetters = () => {
-  const LETTER_POOL = {A:9, B:2, C:2, D:4, E:12, F:2, G:3, H:2, I:9, J:1, K:1, L:4,  
-    M:2, N:6, O:8, P:2, Q:1, R:6, S:4, T:6, U:4, V:2, W:2, X:1, Y:2, Z:1};
-  
   const weightedPool = [];
+
   for (const [key, value] of Object.entries(LETTER_POOL)) {
     weightedPool.push(...Array(value).fill(key));
   }
   
   const hand = [];
+  
   while (hand.length < 10) {
     const letterIndex = Math.floor(Math.random() * weightedPool.length);
     hand.push(weightedPool[letterIndex]);
@@ -18,8 +20,35 @@ export const drawLetters = () => {
 
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  const handCopy = [...lettersInHand];
+  
+  for (let i = 0; i < input.length; i++) {
+    if (handCopy.includes(input[i])) {
+      handCopy.shift();
+    } else {
+      return false;
+    }
+  }
+  return true
 };
+//   const map = {};
+//   for (let char of lettersInHand) {
+//     if (char in map) {
+//       map[char] += 1;
+//     } else {
+//       map[char] = 1;
+//     }
+//   }
+  
+//   for (let char of input.toUpperCase()) {
+//     if((!(char in map)) || map[char] == 0) {
+//       return false;
+//     } else {
+//       map[char] -= 1;
+//     }
+//   }
+//   return true;
+// };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
