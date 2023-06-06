fce16019a38a399e530ec8d5d12b266f88a41f5c
Allie Soliz <solizale@gmail.com>
2023-06-05 22:42:38 -0400

    creates drawLetters() to return an array of 10 random letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,63 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+	// Implement this method for wave 1
+	const LETTER_POOL = {
+		A: 9,
+		B: 2,
+		C: 2,
+		D: 4,
+		E: 12,
+		F: 2,
+		G: 3,
+		H: 2,
+		I: 9,
+		J: 1,
+		K: 1,
+		L: 4,
+		M: 2,
+		N: 6,
+		O: 8,
+		P: 2,
+		Q: 1,
+		R: 6,
+		S: 4,
+		T: 6,
+		U: 4,
+		V: 2,
+		W: 2,
+		X: 1,
+		Y: 2,
+		Z: 1,
+	};
+
+	let arrayResult = [];
+  // letterKeys will hold an array of all of the available letters
+  let letterKeys = Object.keys(LETTER_POOL)
+
+	for (let i = 0; i < 10; i++) {
+		let randomIndex = Math.floor(Math.random() * letterKeys.length);
+    let randomLetter = letterKeys[randomIndex];
+
+    arrayResult.push(randomLetter)
+
+    LETTER_POOL[randomLetter]--;
+
+    if(LETTER_POOL[randomLetter] === 0) {
+      delete LETTER_POOL[randomLetter];
+      letterKeys.splice(randomIndex, 1);
+    }
+	}
+	return arrayResult;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+	// Implement this method for wave 2
+  
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+	// Implement this method for wave 3
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+	// Implement this method for wave 4
 };
