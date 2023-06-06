77d272d73172652be484c758d9645614d78a80d0
Sage Choi <coolcuds@gmail.com>
2023-06-06 04:01:42 -0700

    Added functionality to scoreWord() to calculate a word's score and highestScoreForm() to determine the best word and its score from a list of words

--- src/adagrams.js
+++ src/adagrams.js
@@ -65,9 +65,80 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+    const letterScores = {
+        A: 1,
+        B: 3,
+        C: 3,
+        D: 2,
+        E: 1,
+        F: 4,
+        G: 2,
+        H: 4,
+        I: 1,
+        J: 8,
+        K: 5,
+        L: 1,
+        M: 3,
+        N: 1,
+        O: 1,
+        P: 3,
+        Q: 10,
+        R: 1,
+        S: 1,
+        T: 1,
+        U: 1,
+        V: 4,
+        W: 4,
+        X: 8,
+        Y: 4,
+        Z: 10,
+    };
+
+    if (!word || word === '') {
+        return 0;
+    }
+
+    let points = 0;
+    word = word.toUpperCase();
+
+    for (let i = 0; i < word.length; i++) {
+        if (word.charAt(i) in letterScores) {
+            points += letterScores[word.charAt(i)];
+        }
+    }
+
+    if (word.length > 6) {
+        points += 8;
+    }
+
+    return points
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+    if (!words) {
+        return 0;
+    }
+
+    let bestWord = '';
+    let bestScore = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let score = scoreWord(word);
+
+        if (score > bestScore) {
+            bestWord = word;
+            bestScore = score;
+        } else if (score == bestScore) {
+            if (bestWord.length !== 10 && word.length === 10) {
+                bestWord = word;
+                bestScore = score;
+            } else if (word.length < bestWord.length && bestWord.length !== 10) {
+                bestWord = word;
+                bestScore = score;
+            }
+        }
+    }
+
+    return {'word': bestWord, 'score': bestScore};
 };
