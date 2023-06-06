99f704d9e8cafb5e51633b30e94b6eb7670f7bee
Amanda <jonesamanda996@gmail.com>
2023-06-05 22:01:53 -0400

    score word function and returns zero test

--- src/adagrams.js
+++ src/adagrams.js
@@ -63,36 +63,46 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   const letterValue = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
+    'A': 1,
+    'E': 1,
+    'I': 1,
+    'O': 1,
+    'U': 1,
+    'L': 1,
+    'N': 1,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'D': 2,
+    'G': 2,
+    'B': 3,
+    'C': 3,
+    'M': 3,
+    'P': 3,
+    'F': 4,
+    'H': 4,
+    'V': 4,
+    'W': 4,
+    'Y': 4,
+    'K': 5,
+    'J': 8,
+    'X': 8,
+    'Q': 10,
+    'Z': 10
   };
+
+  if (word.length === 0) {
+    return 0;
+  }
+
   let score = 0;
-  for(let letter in word) {
-    score += letterValue[letter];
+  for(let letter of word) {
+    // const upperCaseLetter = letter.toUpperCase();
+    const points = letterValue[letter.toUpperCase()];
+    score += points; 
+  }
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8; 
   }
   return score;
 };
