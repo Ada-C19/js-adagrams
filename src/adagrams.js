427fef9dbe0439a90d2cfaed954fce8262bf80e6
Elaine Watkins <ewatki.ga@gmail.com>
2023-06-06 17:44:33 -0400

    added notes to drawLetters() function, completed usesAvailableLetters() function and scoreWord() function, finished 'returns a score of 0 if given an empty input' test

--- src/adagrams.js
+++ src/adagrams.js
@@ -8,15 +8,22 @@ export const drawLetters = () => {
 
   let hand = [];
 
+  // Repeat loop until hand is 10
   while (hand.length < 10) {
+    // Generate a random n between 0 and total amount of available tiles
     let randomNumber = Math.floor(Math.random() * cumulativeWeight);
+    // Iterate through letters' length
     for (let i = 0; i < letters.length; i++) {
+      // Continously subtract current weight from cumulative weight
       randomNumber -= weights[i];
+      // When weight reaches 0, remove letter from pool
       if (weights[i] === 0) {
         delete letters[i];
       }
+      // When random n goes below 0, use current i to access the letter at that index, add letter to hand
       if (randomNumber < 0) {
         hand.push(letters[i]);  
+        // Subtract that tile from that letter's weight and stop subtracting
         weights[i] -= 1;
         break;
       }
@@ -25,17 +32,73 @@ export const drawLetters = () => {
   return hand;
 };
 
-
-
-
-
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  const word = input.toUpperCase();
+
+  for (const letter of word) {
+    // Count each letter amount in lettersInHand, if it exists in word
+    const letterCount = lettersInHand.filter(x => x == letter).length;
+
+    // Regex to count each letter amount in word
+    const re = new RegExp(letter, 'g');
+    const count = word.match(re).length;
+
+    // if letter in word does not exist or
+    // letter exists but not enough in bank to make word, return false
+    if (!lettersInHand.includes(letter) || letterCount < count) {
+      return false;
+    } 
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const letterScore = {
+    A: 1, 
+    E: 1, 
+    I: 1, 
+    O: 1, 
+    U: 1, 
+    L: 1, 
+    N: 1, 
+    R: 1, 
+    S: 1, 
+    T: 1,
+    D: 2,
+    G: 2,
+    B: 3,  
+    C: 3, 
+    M: 3, 
+    P: 3,  
+    F: 4,  
+    H: 4, 
+    V: 4, 
+    W: 4, 
+    Y: 4, 
+    K: 5,    
+    J: 8,  
+    X: 8,
+    Q: 10, 
+    Z: 10             
+  };
+  let score = 0;
+
+  if (word.length === 0) {
+    return score;
+  } else {
+    const upperCaseWord = word.toUpperCase();
+    for (const char of upperCaseWord) {
+      score += letterScore[char];
+    }
+    if (word.length >= 7) {
+      score += 8;
+    }
+    return score;
+  }
 };
+
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
