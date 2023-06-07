5c5198c4c854b9d511fe8ef1833fc5567bd85c46
Allie Soliz <solizale@gmail.com>
2023-06-07 13:17:28 -0400

    moves LETTER_POOL to global

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,57 +1,61 @@
+const LETTER_POOL = {
+	A: 9,
+	B: 2,
+	C: 2,
+	D: 4,
+	E: 12,
+	F: 2,
+	G: 3,
+	H: 2,
+	I: 9,
+	J: 1,
+	K: 1,
+	L: 4,
+	M: 2,
+	N: 6,
+	O: 8,
+	P: 2,
+	Q: 1,
+	R: 6,
+	S: 4,
+	T: 6,
+	U: 4,
+	V: 2,
+	W: 2,
+	X: 1,
+	Y: 2,
+	Z: 1,
+};
+
 export const drawLetters = () => {
 	// Implement this method for wave 1
-	const LETTER_POOL = {
-		A: 9,
-		B: 2,
-		C: 2,
-		D: 4,
-		E: 12,
-		F: 2,
-		G: 3,
-		H: 2,
-		I: 9,
-		J: 1,
-		K: 1,
-		L: 4,
-		M: 2,
-		N: 6,
-		O: 8,
-		P: 2,
-		Q: 1,
-		R: 6,
-		S: 4,
-		T: 6,
-		U: 4,
-		V: 2,
-		W: 2,
-		X: 1,
-		Y: 2,
-		Z: 1,
-	};
 
-	let arrayResult = [];
-  // letterKeys will hold an array of all of the available letters
-  let letterKeys = Object.keys(LETTER_POOL)
+	// Creates array to hold 10 letters
+	let handArray = [];
+	// letterKeys will hold an array of all of the available letters
+	let letterKeys = Object.keys(LETTER_POOL);
 
 	for (let i = 0; i < 10; i++) {
+		// Get random letter from letterKeys array and add to handArray
 		let randomIndex = Math.floor(Math.random() * letterKeys.length);
-    let randomLetter = letterKeys[randomIndex];
+		let randomLetter = letterKeys[randomIndex];
+		handArray.push(randomLetter);
 
-    arrayResult.push(randomLetter)
+		// Decrease the count of letter from hash map LETTER_POOL
+		LETTER_POOL[randomLetter]--;
 
-    LETTER_POOL[randomLetter]--;
-
-    if(LETTER_POOL[randomLetter] === 0) {
-      delete LETTER_POOL[randomLetter];
-      letterKeys.splice(randomIndex, 1);
-    }
+		// Checks if letter count is 0
+		if (LETTER_POOL[randomLetter] === 0) {
+			// Delete letter key from hash map and remove letter from letterKeys
+			delete LETTER_POOL[randomLetter];
+			letterKeys.splice(randomIndex, 1);
+		}
 	}
-	return arrayResult;
+	return handArray;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
 	// Implement this method for wave 2
-  
 };
 
 export const scoreWord = (word) => {
