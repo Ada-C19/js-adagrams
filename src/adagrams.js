71223426636bda2248b76f77df024d7ad951b23b
Genesis Quinn <genesis@quinns.io>
2023-06-06 21:57:57 -0400

    Wave 3 Code Tests Fail

--- src/adagrams.js
+++ src/adagrams.js
@@ -67,6 +67,49 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  
+  const points =[]
+  const scoreChart = {
+    A : 1, 
+    B : 3, 
+    C : 3, 
+    D : 2, 
+    E : 1, 
+    F : 4, 
+    G : 2, 
+    H : 4, 
+    I : 1, 
+    J : 8, 
+    K : 5, 
+    L : 1, 
+    M : 3, 
+    N : 1, 
+    O : 1, 
+    P : 3, 
+    Q : 10, 
+    R : 1, 
+    S : 1, 
+    T : 1, 
+    U : 1, 
+    V : 4, 
+    W : 4, 
+    X : 8, 
+    Y : 4, 
+    Z : 10
+}
+
+for (const letter of word){
+  if (scoreChart.includes(letter)){
+    points.push(scoreChart[letter])
+  } let total = points.reduce((a,b)=> a + b, 0);
+  if (word.length >= 7){
+    (total += 8);
+  }
+  return total;
+}
+
+
+
 };
 
 export const highestScoreFrom = (words) => {
