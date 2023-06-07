b97650b7c6c76aac685b5740e02866cb6c18dcec
SF <Sabriaford23@gmail.com>
2023-06-06 19:05:45 -0700

    most of wave 3 completed

--- src/adagrams.js
+++ src/adagrams.js
@@ -75,7 +75,51 @@ return true;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+let upperCaseWord = word.toUpperCase();
+let total = 0;
+let scoreChart = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
+
+if (word.length === 0) {
+  throw 'Complete test';
+  // return 0;
+}
+
+for (let letter of upperCaseWord) {
+  total += scoreChart[letter]
+}
+
+if (word.length >= 7) {
+  total += 8
+}
+
+return total;
 };
 
 export const highestScoreFrom = (words) => {
