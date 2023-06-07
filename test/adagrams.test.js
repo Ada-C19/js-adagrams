5440789e3fb04d80de21bff764b62684b20c8110
Yael Peet <yaelpeet@gmail.com>
2023-06-06 18:54:17 -0700

    wave complete

--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -118,9 +118,11 @@ describe("Adagrams", () => {
         wHiMsY: 17,
       });
     });
-
+      // COMPLETE THIS TEST 
     it("returns a score of 0 if given an empty input", () => {
-      throw "Complete test";
+      expectScores({
+        '':0
+      })
     });
 
     it("adds an extra 8 points if word is 7 or more characters long", () => {
@@ -133,7 +135,7 @@ describe("Adagrams", () => {
     });
   });
 
-  describe.skip("highestScoreFrom", () => {
+  describe("highestScoreFrom", () => {
     it("returns a hash that contains the word and score of best word in an array", () => {
       const words = ["X", "XX", "XXX", "XXXX"];
       const correct = { word: "XXXX", score: scoreWord("XXXX") };
@@ -144,8 +146,8 @@ describe("Adagrams", () => {
     it("accurately finds best scoring word even if not sorted", () => {
       const words = ["XXX", "XXXX", "X", "XX"];
       const correct = { word: "XXXX", score: scoreWord("XXXX") };
-
-      throw "Complete test by adding an assertion";
+      // COMPLETE THIS TEST 
+      expect(highestScoreFrom(words)).toEqual(correct);
     });
 
     describe("in case of tied score", () => {
