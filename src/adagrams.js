8a361881065d79fc18270905655cd396efe8ffe6
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-06-06 21:12:23 -0400

    Adds scoreWord function for Wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,34 +27,34 @@ const letterPool = [
   'Z',
 ]
 
-// {
-//   A: 9, 
-//   B: 2, 
-//   C: 2, 
-//   D: 4, 
-//   E: 12, 
-//   F: 2, 
-//   G: 3, 
-//   H: 2, 
-//   I: 9, 
-//   J: 1, 
-//   K: 1, 
-//   L: 4, 
-//   M: 2, 
-//   N: 6, 
-//   O: 8, 
-//   P: 2, 
-//   Q: 1, 
-//   R: 6, 
-//   S: 4, 
-//   T: 6, 
-//   U: 4, 
-//   V: 2, 
-//   W: 2, 
-//   X: 1, 
-//   Y: 2, 
-//   Z: 1
-// }
+const letterValues = {
+  A: 1, 
+  B: 3, 
+  C: 3, 
+  D: 2, 
+  E: 1, 
+  F: 4, 
+  G: 2, 
+  H: 4, 
+  I: 1, 
+  J: 8, 
+  K: 5, 
+  L: 1, 
+  M: 3, 
+  N: 1, 
+  O: 1, 
+  P: 3, 
+  Q: 10, 
+  R: 1, 
+  S: 1, 
+  T: 1, 
+  U: 1, 
+  V: 4, 
+  W: 4, 
+  X: 8, 
+  Y: 4, 
+  Z: 10
+}
 
 // helper functions
 export const getRandomIndex = (letterPool) => {
@@ -77,6 +77,7 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   const copyLettersInHand = Array.from(lettersInHand);
+  input = input.toUpperCase();
 
   for(let i = 0; i < input.length; ++i){
     if (copyLettersInHand.includes(input[i])){
@@ -91,7 +92,20 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let score = 0;
+  word = word.toUpperCase();
+
+  if (word.length >= 7){
+    score += 8;
+  } else if (!word){
+    return score
+  }
+
+  for(let letter of word){
+    score += letterValues[letter];
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
