71b9df118a6c05632eeb3fc2eb93d03031b7736e
Yvett Jimenez <yvett.nez@gmail.com>
2023-06-13 12:57:42 -0500

    completed wave 1, created function to draw random letters from a pool copy and take pieces out of pool as pieces are added into the hand

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,42 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letters = [
+    'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+    'B', 'B',
+    'C', 'C',
+    'D', 'D', 'D', 'D',
+    'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+    'F', 'F',
+    'G', 'G', 'G',
+    'H', 'H',
+    'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+    'J',
+    'K',
+    'L', 'L', 'L', 'L',
+    'M', 'M',
+    'N', 'N', 'N', 'N', 'N', 'N',
+    'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+    'P', 'P',
+    'Q',
+    'R', 'R', 'R', 'R', 'R', 'R',
+    'S', 'S', 'S', 'S',
+    'T', 'T', 'T', 'T', 'T', 'T',
+    'U', 'U', 'U', 'U',
+    'V', 'V',
+    'W', 'W',
+    'X',
+    'Y', 'Y',
+    'Z'
+];
+  let hand = [];
+  let letters2 = [...letters];
+  while (hand.length < 10) {
+    const randomLetter = letters2[Math.floor(Math.random() * letters2.length)];
+    hand.push(randomLetter);
+    const index = letters2.indexOf(randomLetter);
+    letters2.splice(index, 1);
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
