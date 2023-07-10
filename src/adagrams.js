ad4b58cc71b3b0eabf2e1708ef0aab325d617ad1
Jessica <Yiskah.Scientia@gmail.com>
2023-07-10 17:43:01 -0400

    Wave 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,18 +1,17 @@
-
-
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
-
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
-
-
 const letterDistribution = {
   A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1,
   L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1,
   Y: 2, Z: 1
+};
+
+const letterScores = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
 };
 
 export const drawLetters = () => {
@@ -52,4 +51,45 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
 
   return true;
+};
+
+export const scoreWord = (word) => {
+  const cleanedWord = word.toUpperCase().replace(/[^A-Z]/g, '');
+  let score = 0;
+
+  for (let letter of cleanedWord) {
+    score += letterScores[letter];
+  }
+
+  if (cleanedWord.length >= 7 && cleanedWord.length <= 10) {
+    score += 8;
+  }
+
+  return score;
+};
+
+export const highestScoreFrom = (words) => {
+  let highestScore = 0;
+  let winningWord = '';
+
+  for (const word of words) {
+    const score = scoreWord(word);
+
+    if (score > highestScore) {
+      highestScore = score;
+      winningWord = word;
+    } else if (score === highestScore) {
+      if (winningWord.length === 10) {
+        continue;
+      } else if (word.length === 10) {
+        highestScore = score;
+        winningWord = word;
+      } else if (word.length < winningWord.length) {
+        highestScore = score;
+        winningWord = word;
+      }
+    }
+  }
+
+  return { word: winningWord, score: highestScore };
 };
